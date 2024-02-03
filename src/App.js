2b9004c323c49ec40c5404588baa9aa6bfed26db
jasperAnonymouse <xanthonyalvarezx@gmail.com>
2024-02-03 16:28:00 -0500

    Skeleton Created

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import  Landing  from "./components/Landing"
+import  Projects  from "./components/Projects"
+import  Contact  from "./components/Contact"
+import { Route, Routes } from "react-router-dom"
 function App() {
   return (
-    <Landing/>
+    <>
+       <Routes>
+          <Route exact path="/" element={<Landing />} />
+          <Route exact path="/projects" element={<Projects />} />
+          <Route exact path="/contact" element={<Contact />} />
+        </Routes>
+    </>
   );
 }
 
---
+++ src/components/Contact.jsx
@@ -0,0 +1,13 @@
+
+function Contact() {
+    return (
+      <>
+        <h1>
+          Contact
+        </h1>
+      </>
+    );
+  }
+  
+  export default Contact;
+  
